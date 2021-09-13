1c9f55f65fbcf585d43f168cd871b20b1422f747
eugen.casian <casian.eugen1999@gmail.com>
2021-09-13 16:20:35 +0300

    first

--- Chapter 2/src/Ex29.java
+++
@@ -1,8 +0,0 @@
-public class Ex29 {
-    public static void main(String[] args) {
-        char c;
-        for(c = '!'; c <= 'z'; c++){
-            System.out.printf("The character %c has the value %d%n", c, (int) c);
-        }
-    }
-}
