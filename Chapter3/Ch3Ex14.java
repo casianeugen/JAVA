aead63e9aaef3ed977c14a9f7c68b900974924e9
eugen.casian <casian.eugen1999@gmail.com>
2021-09-20 09:12:46 +0300

    Edit ex 14 Chapter 3

--- Chapter3/Ch3Ex14.java
+++ Chapter3/Ch3Ex14.java
@@ -1,12 +1,15 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
-class Date14 {
+class Date {
     private int month;
     private int day;
     private int year;
 
-    public Date14(int month, int day, int year) {
+    public Date() {
+    }
+
+    public Date(int month, int day, int year) {
         this.month = month;
         this.day = day;
         this.year = year;
@@ -41,7 +44,7 @@ class Date14 {
         SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
         sdf.setLenient(false);
         try{
-            java.util.Date dt1 = sdf.parse(dateString);
+            sdf.parse(dateString);
             System.out.println(dateString);
         } catch (ParseException e) {
             System.out.println(dateString + " Is Invalid Date");
@@ -51,10 +54,16 @@ class Date14 {
 
 public class Ch3Ex14 {
     public static void main(String[] args) {
-        Date14 d1 = new Date14(4,30,1999);
-        Date14 d2 = new Date14(15,30, 2012);
+        Date d1 = new Date(4,30,1999);
+        Date d2 = new Date(15,30, 2012);
+        Date d3 = new Date();
+
+        d3.setDay(10);
+        d3.setMonth(9);
+        d3.setYear(2021);
 
         d1.displayDate(d1.getMonth(), d1.getDay(), d1.getYear());
         d2.displayDate(d2.getMonth(), d2.getDay(), d2.getYear());
+        d3.displayDate(d3.getMonth(), d3.getDay(), d3.getYear());
     }
 }
