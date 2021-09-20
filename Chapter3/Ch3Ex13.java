9ac0c2bc5f10c1321cd2874a53a4c81f8073bf58
eugen.casian <casian.eugen1999@gmail.com>
2021-09-20 09:01:13 +0300

    Edit ex 13 Chapter 3

--- Chapter3/Ch3Ex13.java
+++ Chapter3/Ch3Ex13.java
@@ -3,6 +3,8 @@ class Employee{
     private String lastName;
     private double salary;
 
+    public Employee() {}
+
     public Employee(String firstName, String lastName, double salary) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -43,13 +45,24 @@ public class Ch3Ex13 {
     public static void main(String[] args) {
         Employee emp1 = new Employee("Name", "Surname", 400);
         Employee emp2 = new Employee("Name2", "Surname2", 500);
+        Employee emp3 = new Employee();
+
+        emp3.setFirstName("Name3");
+        emp3.setLastName("Surname3");
+        emp3.setSalary(1000);
 
         //Yearly salary
         System.out.printf("%n%s have yearly salary: %.2f", emp1.getFirstName(), emp1.getSalary() * 12);
         System.out.printf("%n%s have yearly salary: %.2f%n", emp2.getFirstName(), emp2.getSalary() * 12);
+        System.out.printf("%n%s have yearly salary: %.2f%n", emp3.getFirstName(), emp3.getSalary() * 12);
 
         //Yearly salary with raised monthly salary
-        System.out.printf("%n%s have yearly salary: %.2f", emp1.getFirstName(), emp1.raiseSalary(emp1.getSalary()) * 12);
-        System.out.printf("%n%s have yearly salary: %.2f", emp2.getFirstName(), emp2.raiseSalary(emp2.getSalary()) * 12);
+        System.out.printf("%n%s %s have yearly salary: %.2f", emp1.getFirstName(),
+                emp1.getLastName(), emp1.raiseSalary(emp1.getSalary()) * 12);
+        System.out.printf("%n%s %s have yearly salary: %.2f", emp2.getFirstName(),
+                emp2.getLastName(), emp2.raiseSalary(emp2.getSalary()) * 12);
+        System.out.printf("%n%s %s have yearly salary: %.2f", emp3.getFirstName(),
+                emp3.getLastName(), emp3.raiseSalary(emp3.getSalary()) * 12);
+
     }
 }
