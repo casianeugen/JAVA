ea8c61fad382ab5044f54e444d0d3df17f1f5f2e
eugen.casian <casian.eugen1999@gmail.com>
2021-09-20 09:17:25 +0300

    Edit ex 14 Chapter 3

--- Chapter3/Ch3Ex15.java
+++ Chapter3/Ch3Ex15.java
@@ -4,6 +4,9 @@ class Account15 {
     private String name;
     private double balance;
 
+    public Account15() {
+    }
+
     public Account15(String name, double balance) {
         this.name = name;
         if(balance > 0.0)
@@ -29,6 +32,10 @@ class Account15 {
     public double getBalance() {
         return balance;
     }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
 public class Ch3Ex15 {
     public static void displayAccount(Account15 accountToDisplay){
@@ -38,11 +45,17 @@ public class Ch3Ex15 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        Account15 acc3 = new Account15("Name Surname", 50.00);
-        Account15 acc4 = new Account15("Name surname", -7.53);
+        Account15 acc3 = new Account15("Name Surname 3", 50.00);
+        Account15 acc4 = new Account15("Name Surname 4", -7.53);
+        Account15 acc5 = new Account15();
+
+        acc5.setName("Name Surname 5");
+        acc5.setBalance(50.50);
 
         displayAccount(acc3);
         displayAccount(acc4);
+        displayAccount(acc5);
+
         //deposit
         System.out.println("Enter deposit amount for account1: ");
         double depositAmount = input.nextDouble();
@@ -50,8 +63,8 @@ public class Ch3Ex15 {
                 depositAmount);
         acc3.deposit(depositAmount);
 
-        //withdraw acc1
-        System.out.println("Enter amount for withdraw for acc1:");
+        //withdraw acc3
+        System.out.println("Enter amount for withdraw for acc3:");
         double withdrawAmount = input.nextDouble();
         if(withdrawAmount <= acc3.getBalance()){
             acc3.withdraw(withdrawAmount);
@@ -60,8 +73,8 @@ public class Ch3Ex15 {
         else
             System.out.println("Withdraw amount exceeded account balance");
 
-        //withdraw acc2
-        System.out.println("Enter amount for withdraw for acc2:");
+        //withdraw acc4
+        System.out.println("Enter amount for withdraw for acc4:");
         withdrawAmount = input.nextDouble();
         if(withdrawAmount <= acc4.getBalance()){
             acc4.withdraw(withdrawAmount);
@@ -70,7 +83,18 @@ public class Ch3Ex15 {
         else
             System.out.println("Withdraw amount exceeded account balance");
 
+        //withdraw acc5
+        System.out.println("Enter amount for withdraw for acc5:");
+        withdrawAmount = input.nextDouble();
+        if(withdrawAmount <= acc5.getBalance()){
+            acc5.withdraw(withdrawAmount);
+            System.out.println("\nSuccessful withdraw");
+        }
+        else
+            System.out.println("Withdraw amount exceeded account balance");
+
         displayAccount(acc3);
         displayAccount(acc4);
+        displayAccount(acc5);
     }
 }
