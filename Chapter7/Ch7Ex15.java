20a1dfd1b5f77fd647cfa78b30553a6cf24e9682
eugen.casian <casian.eugen1999@gmail.com>
2021-10-22 12:32:01 +0300

    Chapter 7

--- Chapter7/Ch7Ex15.java
+++ Chapter7/Ch7Ex15.java
@@ -1,10 +1,9 @@
 public class Ch7Ex15 {
     public static void main(String[] args) {
-        int[] array = null;
-        if (args == null || args.length == 0)
-            array = new int[10];
-        else
+        int[] array = new int[10];
+        if (args.length != 0)
             array = new int[Integer.parseInt(args[0])];
+        System.out.printf("%s%8s%n", "Index", "Value");
         for (int counter = 0; counter < array.length; counter++)
             System.out.printf("%5d%8d%n", counter, array[counter]);
     }
